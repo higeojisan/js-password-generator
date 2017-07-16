adac1a5c67a532d5d363e315a65e355cca34a11a
m-kouda <makotokoda82@gmail.com>
2017-07-16 16:53:59 +0900

    文字数の指定

--- js/main.js
+++ js/main.js
@@ -3,19 +3,34 @@
 
   var input = document.getElementById('password');
   var btn = document.getElementById('btn');
+  var word_count_ele = document.getElementById('word-count');
   var alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+                   'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-  function generatePassword() {
+
+  function getWordCount() {
+    var word_count = word_count_ele.value;
+    if (word_count >= 4 && word_count <= 20) {
+      return word_count;
+    } else {
+      return false;
+    }
+  }
+
+  function generatePassword(count) {
     var password = '';
-    for (var i = 0; i < 6; i++) {
+    for (var i = 0; i < count; i++) {
       password += alphabets[Math.floor(Math.random() * alphabets.length)];
     }
     return password;
   }
 
   btn.addEventListener('click', function(){
-    var password = generatePassword();
+    var word_count = getWordCount();
+    if (word_count == false) {
+      return;
+    }
+    var password = generatePassword(word_count);
     input.value = password;
   });
 })();
